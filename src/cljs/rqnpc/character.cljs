77daf3f4b1ebf5ae4dd8e90c2a099a40943870bc
Simo Råman <simoraman@gmail.com>
2015-09-19 10:25:03 +0300

    set functions private

--- src/cljs/rqnpc/character.cljs
+++ src/cljs/rqnpc/character.cljs
@@ -1,19 +1,19 @@
 (ns rqnpc.character)
 
-(defn roll [sides]
+(defn- roll [sides]
   (+ 1 (rand-int sides)))
 
 (def abilities [:strength :stamina :power :dexterity :charisma :intelligence])
 
-(defn roll-ability []
+(defn- roll-ability []
   (reduce + (repeat 3 (roll 6))))
 
-(defn generate-abilities []
+(defn- generate-abilities []
   (into
    (hash-map)
    (mapcat (fn [ability] {ability (roll-ability)}) abilities)))
-(defn generate-size [] {:size (+ (roll 6) (roll 6) 6)})
-(defn generate-health [size stamina] {:health (int (/ (+ size stamina) 2))})
+(defn- generate-size [] {:size (+ (roll 6) (roll 6) 6)})
+(defn- generate-health [size stamina] {:health (int (/ (+ size stamina) 2))})
 
 (defn new-npc []
   (let [abilities (generate-abilities)
